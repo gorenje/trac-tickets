9d0d303417093229ec445b0c678353d34c17a7d0
Gerrit Riessen <gerrit.riessen@gmail.com>
2009-12-03 11:29:27 +0100

    added git branching

--- pivotal-tickets.rb
+++ pivotal-tickets.rb
@@ -10,12 +10,6 @@ require 'optparse'
 require 'fileutils'
 require 'activeresource'
 
-def charlimit(str,to=50)
-  str_len = (str || "").length
-  str = (str ? str[0..(to-1)].gsub(/[\n\r]/," ") : "")
-  str_len > to ? str.gsub(/...$/,"...") : str
-end
-
 def git_all_branches
   `git branch -a | colrm 1 2`.split(/\n/)
 end
@@ -24,18 +18,19 @@ def git_exists?
   system('git branch 2>/dev/null >/dev/null')
 end
 
-def git_branch_name(idx, ticket)
+def git_branch_name(idx, story)
   _,_,branch_name = (cnt, all_branches, branch_name_base = 
-    1, git_all_branches, "tick%03d_%s" % [idx, ticket.gsub(/[^[:alpha:][:digit:]]/,'_')])
+    1, git_all_branches, "story%03d_%s" % [idx, 
+                                           story.name.gsub(/[^[:alpha:][:digit:]]/,'_')])
   while all_branches.include?(branch_name)
     branch_name = "%s.v%02d" % [branch_name_base, cnt += 1]
   end
   branch_name
 end
 
-def exit_if_ticket_not_open(ticket)
-  if ticket.nil?
-    puts 'ticket not open or not available'
+def exit_if_story_not_open(story)
+  if story.nil?
+    puts 'story not open or not available'
     exit
   end
 end
@@ -67,7 +62,7 @@ DEFAULT_PROJECT=c["default_project"]
 @open_with_app   = nil
 
 opts = OptionParser.new do |o|
-  o.program_name = 'ruby fb.rb'
+  o.program_name = 'ruby pivotal-tickets.rb'
   o.separator 'Options:'
   o.on('--project PROJECTNAME', '-p', 'Project name') { |@project_name| }
   o.on('--report REPORT_NUM',   '-r', 'Report id') { |@report_id| }
@@ -76,17 +71,17 @@ opts = OptionParser.new do |o|
   
   o.on('--gitrename NUM',       '-m', 'rename current git branch to ticket') {|@gitrename| }
   o.on('--branch NUM',          '-b', 'create a git branch for ticket num') { |@gitticket| }
-  o.on('--show-ticket NUM',     '-s', 'show all info on ticket. Comma separated') do |t| 
-    @show_tickets << t.split(',').collect { |a| a.to_i }
-  end
-  o.on('--filter-on REGEXP',    '-f', 'filter string') { |a| @filter = eval a }
-  o.on('--not-assigned',        '-x', 'show tickets that are not assigned') { |@notassigned| }
+#   o.on('--show-ticket NUM',     '-s', 'show all info on ticket. Comma separated') do |t| 
+#     @show_tickets << t.split(',').collect { |a| a.to_i }
+#   end
+#   o.on('--filter-on REGEXP',    '-f', 'filter string') { |a| @filter = eval a }
+#   o.on('--not-assigned',        '-x', 'show tickets that are not assigned') { |@notassigned| }
   o.on('--open APP',            '-o', 'open ticket with the app. App is passed the URL of the ticket') {  |@open_with_app| }
   o.on('--safari',            nil, 'Open tickets with Safari') { @open_with_app = "Safari" }
-  o.on('--only-tickets NUM',    '-t', 'show only these ticket numbers. good in combinatin with open') do |tickets|
-    @filter = eval(t=("/^#(%s)/" % tickets.split(',').
-                   collect {|a| "%03d" % a.to_i }.join('|')))
-  end
+#   o.on('--only-tickets NUM',    '-t', 'show only these ticket numbers. good in combinatin with open') do |tickets|
+#     @filter = eval(t=("/^#(%s)/" % tickets.split(',').
+#                    collect {|a| "%03d" % a.to_i }.join('|')))
+#   end
   
   o.on_tail('--help', '-h', 'Show this message') do
     puts opts
@@ -137,23 +132,30 @@ tickets,summaries,ticket_rows = [],[],[]
 puts content.split("\n")[0] if @debug
 
 module ToOutput
+  def charlimit(str,to=50)
+    str_len = (str || "").length
+    str = (str ? str[0..(to-1)].gsub(/[\n\r]/," ") : "")
+    str_len > to ? str.gsub(/...$/,"...") : str
+  end
+
   def to_row( overrides = { })
     # 060 does not limit the string to max 60, chars therefore the substring below.
     sprintf("#%07d - (%s) (%s) (%s) [%s] %s\n", 
-            self.id, 
-            charlimit((overrides[:milestone]  || self.current_state|| ""),10) .ljust(10, ' '), 
-            charlimit((overrides[:component]  || self.story_type|| ""),10).ljust(10, ' '), 
-            charlimit((overrides[:owner]      || self.requested_by || ""),7).ljust(7, ' '), 
-            charlimit((overrides[:summary]    || self.name || ""),60).ljust(60, ' '),
-            charlimit(overrides[:description] || self.description), 50)
+      self.id, 
+      charlimit((overrides[:milestone]  || self.current_state|| ""),10) .ljust(10, ' '), 
+      charlimit((overrides[:component]  || self.story_type|| ""),8).ljust(8, ' '), 
+      charlimit((overrides[:owner]      || self.requested_by || ""),9).ljust(9, ' '), 
+      charlimit((overrides[:summary]    || self.name || ""),60).ljust(60, ' '),
+      charlimit(overrides[:description] || self.description), 50)
   end
 end
 
+PT_BASE_URL = "http://www.pivotaltracker.com"
 # create the required story class
 ["Story", "Note", "Task"].each do |class_name|
   eval(<<-EOF % [opts[:pivotal_project_id], opts[:pivotal_api_token]])
     class #{class_name} < ActiveResource::Base
-      self.site = "http://www.pivotaltracker.com/services/v2/projects/%s"
+      self.site = "#{PT_BASE_URL}/services/v2/projects/%s"
       headers['X-TrackerToken'] = '%s'
     end
   EOF
@@ -161,28 +163,35 @@ end
 Story.send(:include, ToOutput)
 
 puts "====>>> Active Story for: #{@project_name} <<<===="
-Story.find(:all, :params => {:project_id => opts[:pivotal_project_id]}).each do |story|
+(stories = Story.
+ find(:all, :params => {:project_id => opts[:pivotal_project_id]}).
+ select do |story|
+   true
+ end).each do |story|
   y story if @debug
   puts story.to_row
+  system("open -a %s %s/stories/%s" % [@open_with_app, 
+                                       PT_BASE_URL, story.id]) if !@open_with_app.nil?
+
 end
 
 cnt=0
-puts "====>>> Tickets for: %s Total %d <<<====" % [@project_name, cnt]
+puts "====>>> Tickets for: %s Total %d <<<====" % [@project_name, stories.size]
 
 ## git stuff. either check out with new branch or rename current branch.
 if @gitticket and git_exists?
   idx = @gitticket.to_i
-  ticket = summaries[idx]
-  exit_if_ticket_not_open(ticket)
-  `git co -b #{git_branch_name(idx,ticket)}`
+  story = Story.find(idx, :params => {:project_id => opts[:pivotal_project_id]})
+  exit_if_story_not_open(story)
+  `git co -b #{git_branch_name(idx,story)}`
   exit
 end
 
 if @gitrename and git_exists?
   idx = @gitrename.to_i
-  ticket = summaries[idx]
-  exit_if_ticket_not_open(ticket)
-  `git branch -m #{git_branch_name(idx,ticket)}`
+  story = Story.find(idx, :params => {:project_id => opts[:pivotal_project_id]})
+  exit_if_story_not_open(story)
+  `git branch -m #{git_branch_name(idx,story)}`
   exit
 end
 
